21e8c20443d433384aa71e2dbc9528d4b942332f
yoon-dh <acttoze1@gmail.com>
2024-01-30 16:59:04 +0900

    Next : parallel route

---
+++ src/app/(beforeLogin)/@modal/page.tsx
@@ -0,0 +1,3 @@
+export default function Modal() {
+    return <div>페러렐 라우트</div>;
+}
---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from '../../../../node_modules/next/navigation';
+
+export default function Login() {
+    redirect('/i/flow/login');
+}
