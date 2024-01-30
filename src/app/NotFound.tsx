dacc84c9872b066d7ca574c255e18e3824d4a061
yoon-dh <acttoze1@gmail.com>
2024-01-30 16:04:25 +0900

    Next : Folder architecture

---
+++ src/app/NotFound.tsx
@@ -0,0 +1,10 @@
+// const NotFound: NextPage = () => {
+//     return (
+//         <div>
+//             <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해보세요</div>
+//             <Link href='/search'>검색</Link>
+//         </div>
+//     );
+// };
+
+// export default NotFound;
